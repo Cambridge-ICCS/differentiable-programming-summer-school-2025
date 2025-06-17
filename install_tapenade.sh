a4e4fcf96f9b327174b7a4d0be4bb4b9369c1545
Joe Wallwork <jw2423@cam.ac.uk>
2025-06-17 09:51:24 +0100

    Add script for installing Tapenade

---
+++ install_tapenade.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create a Python virtual environment and pip install all requirements
+python3 -m venv venv
+source venv/bin/activate
+echo "source $(pwd)/.venv/bin/activate" >>${HOME}/.bashrc
+pip install -r requirements.txt
+
+# Install Tapenade and put it in the path
+wget https://tapenade.gitlabpages.inria.fr/tapenade/distrib/tapenade_3.16.tar
+tar xvf tapenade_3.16.tar
+rm tapenade_3.16.tar
+export PATH=${PWD}/tapenade_3.16/bin:${PATH}
