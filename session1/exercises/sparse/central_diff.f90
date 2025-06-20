cb1f3a40430136aecc54124596b1a7a0bbe110ed
Joe Wallwork <jw2423@cam.ac.uk>
2025-06-20 11:40:23 +0100

    Rework sparse example to be all vector mode

--- session1/exercises/sparse/central_diff.f90
+++ session1/exercises/sparse/central_diff.f90
@@ -2,12 +2,11 @@
 subroutine central_diff(u, approx, h, n)
   implicit none
 
-  integer, intent(in) :: n
-  real, intent(in), dimension(n) :: u
-  real, intent(out), dimension(n) :: approx
-  real, intent(in) :: h
-
-  integer :: i
+  integer, intent(in) :: n                   ! Number of grid points
+  real, intent(in) :: h                      ! Uniform grid spacing
+  real, dimension(n), intent(in) :: u        ! Input vector
+  real, dimension(n), intent(out) :: approx  ! Central difference approximation
+  integer :: i                               ! Dummy index for looping
 
   if (size(u, 1) /= n) then
     print *, "Invalid input array size"
@@ -20,11 +19,15 @@ subroutine central_diff(u, approx, h, n)
 
   do i = 1, n
     if (i == 1) then
+      ! Periodic boundary on the left
       approx(i) = (u(n) - 2 * u(i) + u(i+1)) / h ** 2
     else if (i == n) then
+      ! Periodic boundary on the right
       approx(i) = (u(i-1) - 2 * u(i) + u(1)) / h ** 2
     else
+      ! Interior points
       approx(i) = (u(i-1) - 2 * u(i) + u(i+1)) / h ** 2
     end if
   end do
+
 end subroutine central_diff
