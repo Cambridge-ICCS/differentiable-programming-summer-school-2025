17d22da1270658711439d378746bb4a42e434af0
Niccolò Zanotti <74741715+niccolozanotti@users.noreply.github.com>
2025-07-05 16:59:33 +0100

    Use fortran `module`s instead of old-fashioned `include`s in ODE example
    The former approach was chosen because of apparent limitations of
    tapenade in handling modules. However the introduced modules can be
    differentiated by simply calling tapenade file_name.f90 which will
    create a new module module_name_diff written to a file file_name_d.f90
    by default.
    * theta.f90 --> theta_methods.f90 (module)
      * made public only externally used subroutines
      * used by forward_euler, backward_euler and gradient_descent
      * to differentiate run: tapenade theta_methods.f90
    * cost_function.f90 --> cost.f90 (module)
      * cost_function defined as subroutine
    * update programs to use the above modules accordingly

--- session1/exercises/ode/backward_euler.f90
+++ session1/exercises/ode/backward_euler.f90
@@ -1,8 +1,9 @@
-include "theta.f90"
-
 ! Program for running the backward Euler method to solve the ODE initial value problem
 !   du/dt = u, u(0) = 1
 program backward_euler
+
+  use theta_methods, only: theta_method
+
   implicit none
 
   real, parameter :: theta = 1.0                        ! Backward Euler corresponds to theta = 1.0
