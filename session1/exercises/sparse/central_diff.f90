e1edc7126809115b774bb1d59153fa6977e5fe36
Niccolò Zanotti <74741715+niccolozanotti@users.noreply.github.com>
2025-07-07 11:01:41 +0100

    Move to used fortran `module`s even for `dense` example

--- session1/exercises/sparse/central_diff.f90
+++ session1/exercises/sparse/central_diff.f90
@@ -1,33 +1,41 @@
-! Central difference approximation for the second derivative of a periodic 1D function
-subroutine central_diff(u, approx, h, n)
+module central_difference
+
   implicit none
 
-  integer, intent(in) :: n                   ! Number of grid points
-  real, intent(in) :: h                      ! Uniform grid spacing
-  real, dimension(n), intent(in) :: u        ! Input vector
-  real, dimension(n), intent(out) :: approx  ! Central difference approximation
-  integer :: i                               ! Dummy index for looping
+contains
 
-  if (size(u, 1) /= n) then
-    print *, "Invalid input array size"
-    stop 1
-  end if
-  if (size(approx, 1) /= n) then
-    print *, "Invalid output array size"
-    stop 1
-  end if
-
-  do i = 1, n
-    if (i == 1) then
-      ! Periodic boundary on the left
-      approx(i) = (u(n) - 2 * u(i) + u(i+1)) / h ** 2
-    else if (i == n) then
-      ! Periodic boundary on the right
-      approx(i) = (u(i-1) - 2 * u(i) + u(1)) / h ** 2
-    else
-      ! Interior points
-      approx(i) = (u(i-1) - 2 * u(i) + u(i+1)) / h ** 2
+  ! Central difference approximation for the second derivative of a periodic 1D function
+  subroutine central_diff(u, approx, h, n)
+    implicit none
+  
+    integer, intent(in) :: n                   ! Number of grid points
+    real, intent(in) :: h                      ! Uniform grid spacing
+    real, dimension(n), intent(in) :: u        ! Input vector
+    real, dimension(n), intent(out) :: approx  ! Central difference approximation
+    integer :: i                               ! Dummy index for looping
+  
+    if (size(u, 1) /= n) then
+      print *, "Invalid input array size"
+      stop 1
     end if
-  end do
+    if (size(approx, 1) /= n) then
+      print *, "Invalid output array size"
+      stop 1
+    end if
+  
+    do i = 1, n
+      if (i == 1) then
+        ! Periodic boundary on the left
+        approx(i) = (u(n) - 2 * u(i) + u(i+1)) / h ** 2
+      else if (i == n) then
+        ! Periodic boundary on the right
+        approx(i) = (u(i-1) - 2 * u(i) + u(1)) / h ** 2
+      else
+        ! Interior points
+        approx(i) = (u(i-1) - 2 * u(i) + u(i+1)) / h ** 2
+      end if
+    end do
+  
+  end subroutine central_diff
 
-end subroutine central_diff
+end module central_difference
